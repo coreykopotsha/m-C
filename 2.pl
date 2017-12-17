5a1e94eb5548a5fd728c0500d0c4d63ed5482cac
coreykopotsha <34192427+coreykopotsha@users.noreply.github.com>
2017-12-17 21:03:35 +0000

    Update 2.pl

--- 2.pl
+++ 2.pl
@@ -10,21 +10,20 @@
 ferry(M,C,K):- state(K,M,C,0,0,1,1,[]).
 
 state(K,AM,AC,BM,BC,W,B,L):- between(0,K,TM),
-						between(0,K,TC),  
+						between(0,K,TC),
 						AM2 is AM-TM, AC2 is AC-TC, BM2 is BM+TM, BC2 is BC+TC,
-						not(prefix([AM,AC,BM,BC,W,B],L)),
-						append([AM,AC,BM,BC,W,B], L, L2),
-						tempState(K, AM2, AC2, BM2, BC2, W, B, L2). 
-%write([L2]).
+						tempState(K, AM2, AC2, BM2, BC2, W, B, L).
+
 
 state(_,0,0,BM,BC,-1,-1,L).
 
-tempState(K,AM,AC,BM,BC,W,B, L):- 
+tempState(K,AM,AC,BM,BC,W,B, L):- not(member([AM,AC,BM,BC,W,B],L)),
+						append([AM,AC,BM,BC,W,B], L, L2),
 							legal(AM,AC,W, K), 
 							legal(BM,BC,W, K), 
 							moveWeapon(AM,AC,BM,BC, W, W2), 
 							B2 is B*(-1),
-							state(K, AM, AC, BM, BC, W2, B2,L).
+							state(K, AM, AC, BM, BC, W2, B2,L2).
 
 							
 %tempState(_,0,0,BM,BC,-1,-1,L).					
@@ -35,7 +34,7 @@ legal(M,C,W, K):- ((M >= C, M >= 0, C >= 0) ;
 					(M > 0, C >= 0, W >0)), 
 					legalBoat(M,C,K).
 
-legalBoat(M,C,K):- (M >= C, M >= 0, C >= 0, M + C > 0, M + C =< K) ; 
+legalBoat(M,C,K):- (M >= C, M >= 0, C >= 0, M + C =< K) ; 
 					(M==0, C > 0, K >= C).
 
 
